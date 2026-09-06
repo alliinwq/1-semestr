5163073d65fc38cb28dbfaa1b0ad4d2908cffc53
Alina Melnychuk <alina.melnichuk2010@gmail.com>
2026-09-06 18:01:33 +0300

    laba1

---
+++ laba1.py
@@ -0,0 +1,23 @@
+print ('Hello World!')
+i = 22
+f = 2.22
+s = 'Python'
+m = True
+n = False
+l = [4, 7, 9]
+print (i, type(i))
+print (f, type(f))
+print (s, type(s))
+print (m, type(m))
+print (n, type(n))
+print (l, type(l))
+
+x = 7
+w = 3
+print (x + w)
+print (x - w)
+print (x * w)
+print (x / w)
+print (x // w)
+print (x % w)
+print (x ** w)
