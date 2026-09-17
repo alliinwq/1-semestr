db93252c4ecafd03f999aa16213bbd3ad90bf7b7
Alina Melnychuk <alina.melnichuk2010@gmail.com>
2026-09-17 16:04:01 +0300

    laba1.py
    Added new variable types and input functionality.

--- laba1.py
+++ laba1.py
@@ -1,23 +1,32 @@
-print ('Hello World!')
+print('Hello World!')
+
 i = 22
 f = 2.22
 s = 'Python'
 m = True
 n = False
 l = [4, 7, 9]
-print (i, type(i))
-print (f, type(f))
-print (s, type(s))
-print (m, type(m))
-print (n, type(n))
-print (l, type(l))
+t = (4, 7, 9)
+d = {'name': 'Alina', 'age': 15}
+st = {4, 7, 9}
 
-x = 7
-w = 3
-print (x + w)
-print (x - w)
-print (x * w)
-print (x / w)
-print (x // w)
-print (x % w)
-print (x ** w)
+print(i, type(i))
+print(f, type(f))
+print(s, type(s))
+print(m, type(m))
+print(n, type(n))
+print(l, type(l))
+print(t, type(t))
+print(d, type(d))
+print(st, type(st))
+
+x = int(input('Enter x: '))
+w = int(input('Enter w: '))
+
+print(x + w)
+print(x - w)
+print(x * w)
+print(x / w)
+print(x // w)
+print(x % w)
+print(x ** w)
